d0080fc6da09bfb49ba7c0122891e1e85f3efea1
abadug5793 <bavani7777@gmail.com>
2018-06-20 15:11:16 +0000

    automatically backed up by learn

--- methodwhile.rb
+++ methodwhile.rb
@@ -1,8 +1,12 @@
 def whileee
   levitation_force = 6
 while levitation_force < 10
+<<<<<<< HEAD
 puts  "Wingardium Leviosa"
 levitation_force += 1
+=======
+puts  "WingardiumLeviosa"
+levitation_force=+ 1
 end
 end
 
